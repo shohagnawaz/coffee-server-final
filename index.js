e1c81983649c5da39f3a390e4eb1b38556751450
shohagnawaz <snawazdigital@gmail.com>
2024-08-24 18:37:41 +0600

    update

--- index.js
+++ index.js
@@ -89,6 +89,25 @@ async function run() {
         const result = await userCollection.insertOne(user);
         res.send(result)
     })
+    // 4. patch
+    app.patch('/user', async(req, res) => {
+        const user = req.body;
+        const filter = { email: user.email }
+        const updateDoc = {
+          $set: {
+            lastLoggedAt: user.lastLoggedAt
+          }
+        }
+        const result = await userCollection.updateOne(filter, updateDoc)
+        res.send(result);
+    })
+    // 3. D for delete
+    app.delete('/user/:id', async(req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+        const result = await userCollection.deleteOne(query);
+        res.send(result)
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
